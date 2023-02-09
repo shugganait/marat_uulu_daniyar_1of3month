4baa27b061806d2367141d0e2b7dcf87ddba2a89
shugganait <daniarm209@gmail.com>
2023-02-09 15:17:04 +0600

    Initial commit

---
+++ app/src/main/java/com/kg/marat_uulu_daniyar_1of3month/MainActivity.java
@@ -0,0 +1,36 @@
+package com.kg.marat_uulu_daniyar_1of3month;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText subject, text, addresses;
+    private Button btn_send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initObjects();
+        btn_send.setOnClickListener(view -> {
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setType("text/plain");
+            emailIntent.putExtra(Intent.EXTRA_EMAIL  , addresses.getText().toString());
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+            emailIntent.putExtra(Intent.EXTRA_TEXT   , text.getText().toString());
+
+            startActivity(emailIntent);
+        });
+    }
+
+    private void initObjects(){
+        addresses = findViewById(R.id.et_email);
+        subject = findViewById(R.id.et_theme);
+        text = findViewById(R.id.et_text);
+        btn_send = findViewById(R.id.btn_send);
+    }
+}
